35430b3e4767989d94d582610c86e053b8bc2adb
p22cobrien <p22cobrien@fairfieldprep.org>
2022-02-15 09:44:39 -0500

    add defeat card re-inputs

--- project.py
+++ project.py
@@ -14,77 +14,129 @@ if player[0] == cpu[0]:
     if player[4] < cpu[4]:
         print("defeat {0} < {1}".format(player[4], cpu[4]))
         print("Cpu won:{0} and {1}".format(player[:4],cpu[:4]))
+        for i, v in enumerate(player):
+                if i < 4:
+                    cpu.append(player[v])
+        for i, v in enumerate(cpu):
+            if i < 4:
+                cpu.append(cpu[v])
+        del player[:4]
+        del cpu[:4]
 
     if player[4] == cpu[4]:
         print("WAR")
         print("drawing three more cards")
 
         if player[8] > cpu[8]:
-            print("victory {0} > {1}".format(player[4], cpu[4]))
-            print("You won:{0} and {1}".format(player[:4],cpu[:4]))
+            print("victory {0} > {1}".format(player[8], cpu[8]))
+            print("You won:{0} and {1}".format(player[:8],cpu[:8]))
 
         if player[8] < cpu[8]:
-            print("defeat {0} < {1}".format(player[4], cpu[4]))
-            print("Cpu won:{0} and {1}".format(player[:4],cpu[:4]))
-
+            print("defeat {0} < {1}".format(player[8], cpu[8]))
+            print("Cpu won:{0} and {1}".format(player[:8],cpu[:8]))
+            for i, v in enumerate(player):
+                if i < 8:
+                    cpu.append(player[v])
+            for i, v in enumerate(cpu):
+                if i < 8:
+                    cpu.append(cpu[v])
+            del player[:8]
+            del cpu[:8]
         if player[8] == cpu[8]:
-            print
+            print("WAR")
+            print("drawing three more cards")
+
 
             if player[12] < cpu[12]:
-                print("defeat {0} < {1}".format(player[4], cpu[4]))
-                print("Cpu won:{0} and {1}".format(player[:4],cpu[:4]))
-
+                print("defeat {0} < {1}".format(player[12], cpu[12]))
+                print("Cpu won:{0} and {1}".format(player[:12],cpu[:12]))
+                for i, v in enumerate(player):
+                    if i < 12:
+                        cpu.append(player[v])
+                    for i, v in enumerate(cpu):
+                        if i < 12:
+                            cpu.append(cpu[v])
+                        del player[:12]
+                        del cpu[:12]
             if player[12] > cpu[12]:
-                print("victory {0} > {1}".format(player[4], cpu[4]))
-                print("You won:{0} and {1}".format(player[:4],cpu[:4]))
+                print("victory {0} > {1}".format(player[12], cpu[12]))
+                print("You won:{0} and {1}".format(player[:12],cpu[:12]))
                 
             if player[12] == cpu[12]:
                 print("WAR")
                 print("drawing three additional cards")
 
                 if player[16] < cpu[16]:
-                    print("defeat {0} < {1}".format(player[4], cpu[4]))
-                    print("Cpu won:{0} and {1}".format(player[:4],cpu[:4]))
-
+                    print("defeat {0} < {1}".format(player[16], cpu[16]))
+                    print("Cpu won:{0} and {1}".format(player[:16],cpu[:16]))
+                    for i, v in enumerate(player):
+                        if i < 16:
+                            cpu.append(player[v])
+                    for i, v in enumerate(cpu):
+                        if i < 16:
+                            cpu.append(cpu[v])
+                    del player[:16]
+                    del cpu[:16]
                 if player[16] > cpu[16]:
-                    print("victory {0} > {1}".format(player[4], cpu[4]))
-                    print("You won:{0} and {1}".format(player[:4],cpu[:4]))
+                    print("victory {0} > {1}".format(player[16], cpu[16]))
+                    print("You won:{0} and {1}".format(player[:16],cpu[:16]))
 
                 if player[16] == cpu[16]:
                     print("WAR")
                     print("drawing three additional cards")
                     
                     if player[20] < cpu[20]:
-                        print("defeat {0} < {1}".format(player[4], cpu[4]))
-                        print("Cpu won:{0} and {1}".format(player[:4],cpu[:4]))
-
+                        print("defeat {0} < {1}".format(player[20], cpu[20]))
+                        print("Cpu won:{0} and {1}".format(player[:20],cpu[:20]))
+                        for i, v in enumerate(player):
+                            if i < 20:
+                                cpu.append(player[v])
+                        for i, v in enumerate(cpu):
+                            if i < 20:
+                                cpu.append(cpu[v])
+                        del player[:20]
+                        del cpu[:20]
                     if player[20] > cpu[20]:
-                        print("victory {0} > {1}".format(player[4], cpu[4]))
-                        print("You won:{0} and {1}".format(player[:4],cpu[:4]))
+                        print("victory {0} > {1}".format(player[20], cpu[20]))
+                        print("You won:{0} and {1}".format(player[:20],cpu[:20]))
 
                     if player[20] == cpu[20]:
                         print("WAR")
                         print("drawing three additional cards")
 
                         if player[24] < cpu[24]:
-                            print("defeat {0} < {1}".format(player[4], cpu[4]))
-                            print("Cpu won:{0} and {1}".format(player[:4],cpu[:4]))
-
+                            print("defeat {0} < {1}".format(player[24], cpu[24]))
+                            print("Cpu won:{0} and {1}".format(player[:24],cpu[:24]))
+                            for i, v in enumerate(player):
+                                if i < 24:
+                                    cpu.append(player[v])
+                            for i, v in enumerate(cpu):
+                                if i < 24:
+                                    cpu.append(cpu[v])
+                            del player[:24]
+                            del cpu[:24]
                         if player [24] > cpu[24]:
-                            print("victory {0} > {1}".format(player[4], cpu[4]))
-                            print("You won:{0} and {1}".format(player[:4],cpu[:4]))
+                            print("victory {0} > {1}".format(player[24], cpu[24]))
+                            print("You won:{0} and {1}".format(player[:24],cpu[:24]))
 
                         if player[24] == cpu[24]:
                             print("WAR")
                             print("drawing three additional cards")
 
                             if player[26] < cpu[26]:
-                                print("defeat {0} < {1}".format(player[4], cpu[4]))
-                                print("Cpu won:{0} and {1}".format(player[:4],cpu[:4]))
-
+                                print("defeat {0} < {1}".format(player[26], cpu[26]))
+                                print("Cpu won:{0} and {1}".format(player[:26],cpu[:26]))
+                                for i, v in enumerate(player):
+                                    if i < 26:
+                                        cpu.append(player[v])
+                                for i, v in enumerate(cpu):
+                                    if i < 26:
+                                        cpu.append(cpu[v])
+                                del player[:26]
+                                del cpu[:26]
                             if player[26] > cpu[26]:
-                                print("victory {0} > {1}".format(player[4], cpu[4]))
-                                print("You won:{0} and {1}".format(player[:4],cpu[:4]))
+                                print("victory {0} > {1}".format(player[26], cpu[26]))
+                                print("You won:{0} and {1}".format(player[:26],cpu[:26]))
 
 elif player[0] > cpu[0]:
     print("victory {0} > {1}".format(player[0], cpu[0]))
