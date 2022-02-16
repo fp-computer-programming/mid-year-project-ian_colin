061ed232d47b03ee400fa0c8ba3e5b589bef1007
P22inolan <p22inolan@fairfieldprep.org>
2022-02-16 10:11:23 -0500

    WIP 2/16

--- ian_work.py
+++ ian_work.py
@@ -16,11 +16,33 @@ player_len = len(player)
 cpu_len = len(cpu)
 print(player)
 print(cpu)
-print("You Have: {0} cards.".format(player_len)); sleep
+print("You Have: {0} cards.".format(player_len)); sleep(1.5)
 print("I Have: {0} cards.".format(cpu_len)); sleep(1.5)
 print("The deck is shuffled. Let's Play!"); sleep(2)
 
-while player != [] or cpu != []:
+while player != [] and cpu != []:
+    if player[0] > cpu[0]:
+        print("victory {0} > {1}".format(player[0], cpu[0]))
+        player.append(player[0])
+        player.append(cpu[0])
+        del player[0]
+        del cpu[0]
+        print(("You have {0} cards.").format(len(player)))
+        print(('I have {0} cards.').format(len(cpu))); sleep(2)
+        print(player)
+        print(cpu)
+    
+    if player[0] < cpu[0]:
+        print("loss {0} < {1}".format(player[0], cpu[0]))
+        cpu.append(cpu[0])
+        cpu.append(player[0])
+        del cpu[0]
+        del player[0]
+        print(("You have {0} cards.").format(len(player)))
+        print(('I have {0} cards.').format(len(cpu))); sleep(2)
+        print(player)
+        print(cpu)
+
     if player[0] == cpu[0]:
         print("WAR"); sleep(2)
         print("drawing three additional cards"); sleep(2)
@@ -31,61 +53,55 @@ while player != [] or cpu != []:
                 print("WAR"); sleep(2)
                 print("drawing even more cards"); sleep(2)
             if player[8] > cpu[8]:
-                print("victory {0} > {1}".format(player[8], cpu[8])); sleep(2)
+                print("victory {0} > {1}".format(player[8], cpu[8]))
                 player.extend(player[0:9])
                 player.extend(cpu[0:9])
-                del player[:8]
-                del cpu[:8]
+                del player[:9]
+                del cpu[:9]
+                print(("You have {0} cards.").format(len(player)))
+                print(('I have {0} cards.').format(len(cpu))); sleep(2)
                 print(player)
                 print(cpu)
+                continue
             if player[8] < cpu[8]:
-                print("defeat {0} < {1}".format(player[8], cpu[8])); sleep(2)
+                print("defeat {0} < {1}".format(player[8], cpu[8]))
                 cpu.extend(player[0:9])
                 cpu.extend(cpu[0:9])
                 del player[:9]
                 del cpu[:9]
+                print(("You have {0} cards.").format(len(player)))
+                print(('I have {0} cards.').format(len(cpu))); sleep(2)
                 print(player)
                 print(cpu)
+                continue
         if player[4] > cpu[4]:
-            print("victory {0} > {1}".format(player[4], cpu[4])); sleep(2)
+            print("victory {0} > {1}".format(player[4], cpu[4]))
             player.extend(player[0:5])
             player.extend(cpu[0:5])
             del player[:5]
             del cpu[:5]
+            print(("You have {0} cards.").format(len(player)))
+            print(('I have {0} cards.').format(len(cpu))); sleep(2)
             print(player)
             print(cpu)
+            continue
         if player[4] < cpu[4]:
-            print("defeat {0} < {1}".format(player[4], cpu[4])); sleep(2)
+            print("defeat {0} < {1}".format(player[4], cpu[4]))
             cpu.extend(cpu[0:5])
             cpu.extend(player[0:5])
             del player[:5]
             del cpu[:5]
+            print(("You have {0} cards.").format(len(player)))
+            print(('I have {0} cards.').format(len(cpu))); sleep(2)
             print(player)
             print(cpu)
-
-
-    if player[0] > cpu[0]:
-        print("victory {0} > {1}".format(player[0], cpu[0])); sleep(2)
-        player.append(player[0])
-        player.append(cpu[0])
-        del player[0]
-        del cpu[0]
-        print(player)
-        print(cpu)
-            
-    if player[0] < cpu[0]:
-        print("loss {0} < {1}".format(player[0], cpu[0])); sleep(2)
-        cpu.append(cpu[0])
-        cpu.append(player[0])
-        del cpu[0]
-        del player[0]
-        print(player)
-        print(cpu)
-
-if player == []:
-    print("I win! You lose!"); sleep(1.5)
-if cpu == []:
-    print("You win! I lose!"); sleep(1.5)
+            continue
+    if player == []:
+        print("I win! You lose, {0}!").format(name); sleep(1.5)
+        break
+    if cpu == []:
+        print("You win, {0}! I lose!").format(name); sleep(1.5)
+        break
 
 retry = input("Would you like to play again? (Y/N)")
 print(retry)
